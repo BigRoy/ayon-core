415b79acdf87d648f1ca03fb330397d0a20a1f52
Roy Nieterau <roy_nieterau@hotmail.com>
2024-03-27 01:01:51 +0100

    Improve validation message

--- client/ayon_core/plugins/publish/validate_version.py
+++ client/ayon_core/plugins/publish/validate_version.py
@@ -24,22 +24,23 @@ class ValidateVersion(pyblish.api.InstancePlugin):
         if latest_version is not None and int(version) <= int(latest_version):
             # TODO: Remove full non-html version upon drop of old publisher
             msg = (
-                "Version '{0}' from instance '{1}' that you are "
-                "trying to publish is lower or equal to an existing version "
-                "in the database. Version in database: '{2}'."
+                "Version {0} from instance '{1}' that you are "
+                "trying to publish is lower or equal to existing version: "
+                "{2}.\n"
                 "Please version up your workfile to a higher version number "
-                "than: '{2}'."
+                "than {2}."
             ).format(version, instance.data["name"], latest_version)
 
-            msg_html = (
-                "Version <b>{0}</b> from instance <b>{1}</b> that you are "
+            msg_markdown = (
+                "## Higher version of publish already exists\n"
+                "Version **{0}** from instance **{1}** that you are "
                 "trying to publish is lower or equal to an existing version "
-                "in the database. Version in database: <b>{2}</b>.<br><br>"
+                "in the database. Version in database: **{2}**.\n\n"
                 "Please version up your workfile to a higher version number "
-                "than: <b>{2}</b>."
+                "than: **{2}**."
             ).format(version, instance.data["name"], latest_version)
             raise PublishValidationError(
                 title="Higher version of publish already exists",
                 message=msg,
-                description=msg_html
+                description=msg_markdown
             )
