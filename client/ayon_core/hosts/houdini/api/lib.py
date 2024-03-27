0768ad612efe1b72708b8f22c63910b3f57e2958
Roy Nieterau <roy_nieterau@hotmail.com>
2024-03-27 01:04:15 +0100

    Houdini: Reset frame range, do not include handles

--- client/ayon_core/hosts/houdini/api/lib.py
+++ client/ayon_core/hosts/houdini/api/lib.py
@@ -473,11 +473,13 @@ def reset_framerange(fps=True, frame_range=True):
             log.warning("No edit information found for '%s'", folder_path)
             return
 
-        handle_start = folder_attributes.get("handleStart", 0)
-        handle_end = folder_attributes.get("handleEnd", 0)
-
-        frame_start -= int(handle_start)
-        frame_end += int(handle_end)
+        # COLORBLEED Edit: Do not include handles
+        # TODO(Colorbleed): Make this a project setting
+        # handle_start = folder_attributes.get("handleStart", 0)
+        # handle_end = folder_attributes.get("handleEnd", 0)
+        #
+        # frame_start -= int(handle_start)
+        # frame_end += int(handle_end)
 
         # Set frame range and FPS
         hou.playbar.setFrameRange(frame_start, frame_end)
