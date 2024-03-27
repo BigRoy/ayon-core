8b1e6a0540d86c9e51eb6e40914dd88136ac23a6
Roy Nieterau <roy_nieterau@hotmail.com>
2024-03-27 21:07:14 +0100

    Use `lib.find_active_network` to decide where to create the loaded container

--- client/ayon_core/hosts/houdini/plugins/load/load_usd_reference.py
+++ client/ayon_core/hosts/houdini/plugins/load/load_usd_reference.py
@@ -38,7 +38,11 @@ class USDReferenceLoader(load.LoaderPlugin):
         node_name = "{}_{}".format(namespace, name) if namespace else name
 
         # Create USD reference
-        container = stage.createNode("reference", node_name=node_name)
+        network = lib.find_active_network(
+            category=hou.lopNodeTypeCategory(),
+            default="/stage"
+        )
+        container = network.createNode("reference", node_name=node_name)
         container.setParms({"filepath1": file_path})
         container.moveToGoodPosition()
 
