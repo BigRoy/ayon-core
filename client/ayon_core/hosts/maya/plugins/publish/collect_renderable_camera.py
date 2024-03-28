2dc14109cddf9980482dc34d6a092161e3852f04
Roy Nieterau <roy_nieterau@hotmail.com>
2024-03-28 18:18:53 +0100

    Improve log

--- client/ayon_core/hosts/maya/plugins/publish/collect_renderable_camera.py
+++ client/ayon_core/hosts/maya/plugins/publish/collect_renderable_camera.py
@@ -25,8 +25,7 @@ class CollectRenderableCamera(pyblish.api.InstancePlugin):
         renderable = [cam for cam in cameras if
                       get_attr_in_layer("{}.renderable".format(cam), layer)]
 
-        self.log.debug(
-            "Found renderable cameras %s: %s", len(renderable), renderable
-        )
+        self.log.debug("Found %s renderable cameras in layer '%s': %s",
+                       len(renderable), layer, ", ".join(renderable))
 
         instance.data["cameras"] = renderable
